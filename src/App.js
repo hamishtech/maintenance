c1d186482edaf63b306e4587ba07206533fe445f
HB-1001 <75037684+HB-1001@users.noreply.github.com>
2021-06-29 12:46:47 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Maintenance Mode
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Unfortunately, the website has been discontinued due to lack of traction.</p>
       </header>
     </div>
   );
